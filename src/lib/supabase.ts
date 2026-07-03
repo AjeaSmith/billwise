3700603006c7b9741094e68759675361192ba190
ajea <smithajea@rocketmail.com>
2026-07-03 19:23:17 -0400

    fix: switch supabase oauth flow type to pkce for mobile

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,6 +4,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY,
+  {
+    auth: {
+      detectSessionInUrl: true,
+      flowType: 'pkce',
+    },
+  },
 )
 
 export default supabase
